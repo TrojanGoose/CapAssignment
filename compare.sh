bee4cde9bb94f95d709f9ab82973838645efa0ab
Godwin Nana Kwame <71357880+TrojanGoose@users.noreply.github.com>
2022-05-23 21:46:33 +0000

    Add files via upload

---
+++ compare.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# https://github.com/TrojanGoose
+
+read X
+read Y
+
+if (($X < $Y))
+then 
+	echo "X is less than Y"
+elif (($X > $Y))
+then
+	echo "X is greater than Y"
+else
+	echo "X is equal to Y"
+fi
